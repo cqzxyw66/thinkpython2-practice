1f86f3a6a05cd5eb6b7739c3e24a8b1491e473b8
yangwei <cqzxyw66@gmail.com>
2026-02-23 19:17:02 +0800

    20260223v02 done 09_08

--- 09_08_practice.py
+++ 09_08_practice.py
@@ -1,13 +1,20 @@
-def detect_word_length(word):
-    if len(word) >= 4:
-        return True
-    
-def is_reversible(word):
+def is_palindrome(word):
     i = 0
     j = len(word) - 1
-    new_word = str(word)
     while i < j:
-        if new_word[i] == new_word[j]:
-            return True
+        if word[i] != word[j]:
+            return False
         i = i + 1
         j = j - 1
+    return True
+
+def main():
+    for i in range(1, 1000000, 1):
+        str_number = str(i)
+        length = len(str_number)
+        k = 4
+        while length > 3 and k <= length:
+            if is_palindrome(str_number[-k:]):
+                print(i)
+            k = k + 1
+main()
