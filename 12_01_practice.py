8ef19d56b7bc139e1e9a3f33b7323d6b87a8623e
yangwei-thinkpad <cqzxyw66@gmail.com>
2026-03-06 00:17:33 +0800

    2026-03-05 练习

---
+++ 12_01_practice.py
@@ -0,0 +1,26 @@
+def most_frequest(x):
+    # 将x里面的字母，算出出现出现，赋值给字典j
+    j = dict()
+    for i in x:
+        j[i] = j.get(i, 0) + 1
+    # 将字典j进行反转，得到字典k
+    k = dict()
+    for key in j:
+        val = j[key]
+        if val not in k:
+            k[val] = [key]
+        else:
+            k[val].append(key)
+    # 对字典k的key进行倒序
+    sorted_k = sorted(k, reverse=True)
+    # 新建字符串result, result的取值就等于key(出现次数) * value(字母)
+    result = str()
+    for m in sorted_k:
+        for n in k[m]:
+            result += m * n
+    return result
+
+fin = open('words.txt')
+for i in fin:
+    words = i.strip()
+    print(most_frequest(words))
