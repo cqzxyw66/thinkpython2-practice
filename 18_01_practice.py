0504eebf0e4be95943799f5134a4cac178736171
yangwei-thinkpad <cqzxyw66@gmail.com>
2026-04-01 22:38:32 +0800

    2026-04-01 写了一个斗地主发牌，为自己骄傲!

--- 18_01_practice.py
+++ 18_01_practice.py
@@ -17,13 +17,25 @@ class Card:
         t2 = other.suit, other.rank
         return t1 < t2
     
+class flower_cards(Card):
+    flower_names = ['小王','大王','花牌']
+    def __init__(self, flower=0):
+        self.flower = flower
+    def __str__(self):
+        return '%s' % (flower_cards.flower_names[self.flower])
+    
 class Deck:
-    def __init__(self):
+    def __init__(self, flower=0):
         self.cards = []
         for suit in range(4):
             for rank in range(1, 14):
                 card = Card(suit, rank)
                 self.cards.append(card)
+        # flower_cards_new = flower_cards()
+        if flower != 0:
+            for color in range(3):
+                flower_card = flower_cards(color)
+                self.cards.append(flower_card)
 
     def __str__(self):
         res = []
@@ -52,27 +64,59 @@ class Deck:
 
     def deal_hands(self, persons, one_hand_card_nums):
         a = []
-        for i in range(persons):
+        for i in range(persons + 1):
             a.append(Hand())
         for i in range(one_hand_card_nums):
-            j = 0
+            # j = 0
             for i in range(persons):
                 self.move_cards(a[i], 1)
-                j += 1
+                # j += 1
+        self.move_cards(a[-1], len(self.cards))
         return a
+    
+    def cutting_cards(self):
+        cutting_nums = random.randint(3, 50)
+        bright_card = self.cards[cutting_nums]
+        return bright_card
 
 class Hand(Deck):
     def __init__(self, lable=''):
         self.cards = []
         self.lable = lable
 
-all_cards = Deck()
-all_cards.shuffle()
+# all_cards = Deck(1)
+# king_cards = flower_cards()
+# all_cards.shuffle()
+# t = all_cards.cutting_cards()
 
-print(all_cards)
-x = all_cards.deal_hands(4, 13)
-j = 0
-for i in x:
-    print(f'---第{j+1}家牌：---', '\n', end = '')
-    print(i)
-    j +=1
+# x = all_cards.deal_hands(3, 17)
+# j = 0
+# for i in x:
+#     if j <= 2:
+#         print(f'---第{j+1}家牌：---', '\n', end = '')
+#         print(i)
+#         j +=1
+#     else:
+#         print(f'---底牌：---', '\n', end = '')
+#         print(i)
+
+def dou_di_zhu_fapai():
+    all_cards = Deck(1)
+    all_cards.shuffle()
+    t = all_cards.cutting_cards()
+    print('地主牌为%s' % t)
+    x = all_cards.deal_hands(3, 17)
+    j = 0
+    for i in x:
+        if j <= 2:
+            if t in i.cards:
+                print(f'---地主牌为第{j+1}家所有，他具有优先叫牌权：---', '\n', end = '')
+                print(i)
+            else:
+                print(f'---第{j+1}家牌：---', '\n', end = '')
+                print(i)
+        j += 1
+    print(f'---底牌：---', '\n', end = '')
+    print(x[-1])
+
+dou_di_zhu_fapai()
